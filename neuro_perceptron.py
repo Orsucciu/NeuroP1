c076738e4541261b55e515ab6beaec5b84a3ff6c
Orsu <Orsucciu@users.noreply.github.com>
2018-03-23 03:46:27 +0100

    enough is enough

--- neuro_perceptron.py
+++ neuro_perceptron.py
@@ -12,7 +12,18 @@ retu.createConnexion([0, 3], [1, 1], 0)
 retu.setAllThresholdTo(1)
 retu.generateGraph()
 
-retu.trainPerceptron(retu.processText("and.txt"), 0.01)
+retu.trainPerceptron(retu.processText("and.txt"), 0.01) # this exemple deoesn't work like expected
 retu.exploitPerceptron(retu.processText("and.txt"))
 #retu.trainPerceptron(retu.processText("one.txt"), 1)
 #retu.exploitPerceptron(retu.processText("one.txt"))
+
+retu = Red("perceptron2")
+retu.initCapas([2, 1])
+retu.connectOneToAll(0)
+retu.addCell(0, "bias0", 1)
+retu.createConnexion([0, 2], [1, 0], 0)
+retu.setAllThresholdTo(1)
+retu.generateGraph()
+
+retu.trainPerceptron(retu.processText("one.txt"), 1)
+retu.exploitPerceptron(retu.processText("one.txt"))
